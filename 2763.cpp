728a777544c76a104142e9fa449078fd0ab6e729
Mahmud Hasan Ratul <126283368+Ratul-DIU@users.noreply.github.com>
2023-04-08 01:30:18 +0600

    Add files via upload

---
+++ 2763.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() 
+{
+ char x1,x2,x3,d1,y1,y2,y3,d2,z1,z2,z3,d3,D1,D2;
+ scanf("%c %c %c %c %c %c %c %c %c %c %c %c %c %c ",&x1,&x2,&x3,&d1,&y1,&y2,&y3,&d2,&z1,&z2,&z3,&d3,&D1,&D2);
+ printf("%c%c%c\n",x1,x2,x3);
+  printf("%c%c%c\n",y1,y2,y3);
+   printf("%c%c%c\n",z1,z2,z3);
+    printf("%c%c\n",D1,D2);
+    return 0;
+}
