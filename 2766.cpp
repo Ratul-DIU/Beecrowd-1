deba73ed08158fababe1d66fa365b6c084922259
Mahmud Hasan Ratul <126283368+Ratul-DIU@users.noreply.github.com>
2023-05-04 01:41:05 +0600

    Add files via upload

---
+++ 2766.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char names[10][41]; 
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%s", names[i]);
+    }
+
+    printf("%s\n", names[2]); 
+    printf("%s\n", names[6]); 
+    printf("%s\n", names[8]); 
+
+    return 0;
+} 
+
